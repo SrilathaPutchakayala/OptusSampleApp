52fbc31d2f8b700d5d90dd1ad1af7fe920d7a0c2
Srilatha Putchakayala <SPutchakayala>
2020-06-11 10:31:53 +1000

    Removed Commented lines and Log Statement

--- app/src/main/java/com/optus/sample/utils/BindingAdapters.kt
+++ app/src/main/java/com/optus/sample/utils/BindingAdapters.kt
@@ -1,6 +1,5 @@
 package com.optus.sample.utils
 
-import android.util.Log
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import com.squareup.picasso.Picasso
@@ -10,13 +9,8 @@ object BindingAdapters {
     @BindingAdapter("bind:imageUrl")
     @JvmStatic
     fun loadImage(view: ImageView, imageUrl: String?) {
-        Log.v("====","Inside imageUrl ${imageUrl}")
         if (imageUrl != null && imageUrl.isNotEmpty()){
-            Picasso.get()
-                .load(imageUrl).into(view)
+            Picasso.get().load(imageUrl).into(view)
         }
-        //.placeholder(R.drawable.placeholder)
     }
-
-
 }
